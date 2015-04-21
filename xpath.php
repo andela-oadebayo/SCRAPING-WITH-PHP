9b754b26b511e3a29f81074d91e29dd9327deb4f
andela-oadebayo <oluwadamilola.adebayo@andela.co>
2015-04-21 14:48:16 +0100

    Abadoning this project for now

--- xpath.php
+++ xpath.php
@@ -12,6 +12,32 @@
 
     $dllBook = array();
     function returnXPathObject($item){
-
+        $xmlPageDom = new DOMDocument();
+        @$xmlPageDom->loadHTML($item);
+        $xmlPageXPath = new DOMXPath($xmlPageDom);
+        return $xmlPageXPath;
     }
+
+    $ddlPage = curlGet('http://bookdl.com/');
+    $ddlPageXPath = returnXPathObject($ddlPage);
+    $title = $ddlPageXPath->query('//h1');
+
+    if($title->length > 0){
+        $ddlBook['title'] = $title->item(0)->nodeValue;
+    }
+
+    $img = $ddlPageXPath->query('//img[@class="bookcover"]');
+
+    if($img->length > 0){
+        $ddlBook['img'] = $img->item(0)->nodeValue;
+    }
+
+    $bookInfo = $ddlPageXPath->query('//div[@class="iteminfo"]');
+
+    if($bookInfo->length > 0){
+        $ddlBook['bookinfo'] = trim($bookInfo->item(0)->nodeValue);
+    }
+
+    print_r($ddlBook);
+
 ?>
